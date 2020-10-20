a886c9e36b4a6778f2a03d4aa9bcfa5784aeec8f
Chris <Chris@MacBook-Pro-5.fios-router.home>
2020-10-20 18:28:43 -0400

    added rudimentary testing

--- test_deps.ts
+++ test_deps.ts
@@ -1,6 +1,12 @@
 // deps only used in testing and not in production
+
+// Deno Tests
 export {
   assertEquals,
   assertNotEquals,
 } from "https://deno.land/std@0.74.0/testing/asserts.ts";
 
+// SuperOak
+export { superoak } from 'https://deno.land/x/superoak@2.3.1/mod.ts';
+export { describe, it } from 'https://deno.land/x/superoak@2.3.1/test/utils.ts';
+export { expect } from 'https://deno.land/x/superoak@2.3.1/test/deps.ts';
