32dc6488e5d11d38e0f2006266f024c21ff935f4
alicefu <69636893+alicejfu@users.noreply.github.com>
2020-10-23 11:04:21 -0400

    fixed session ver

--- deps.ts
+++ deps.ts
@@ -1,6 +1,9 @@
 // Standard Library Dependencies
 export { join, dirname } from 'https://deno.land/std@0.74.0/path/mod.ts';
-export * as log from 'https://deno.land/std@0.74.0/log/mod.ts'
+export * as log from 'https://deno.land/std@0.74.0/log/mod.ts';
+
+// for creating sid
+export { v4 } from 'https://deno.land/std/uuid/mod.ts';
 
 // Third Party Dependencies
 
@@ -18,10 +21,10 @@ export { config } from 'https://deno.land/x/dotenv/mod.ts';
 export { MongoClient } from 'https://deno.land/x/mongo@v0.12.1/mod.ts';
 
 // deno session
-export { Session, SessionData } from 'https://deno.land/x/session@v1.0.0/mod.ts';
+export { Session, SessionData } from 'https://deno.land/x/session@1.1.0/mod.ts';
 
 // react
-export { default as React } from "https://dev.jspm.io/react@16.14.0";
-export { default as ReactDOMServer } from "https://dev.jspm.io/react-dom@16.14.0/server";
+export { default as React } from 'https://dev.jspm.io/react@16.14.0';
+export { default as ReactDOMServer } from 'https://dev.jspm.io/react-dom@16.14.0/server';
 
 // import * as bcrypt from "https://deno.land/x/bcrypt@v0.2.4/mod.ts"  // in case we want bcrypt
