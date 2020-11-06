fdfe525ea74b56f01cca0174597c2551064d9f37
alicefu <69636893+alicejfu@users.noreply.github.com>
2020-11-05 20:10:59 -0500

    awaits, closure with onyx

--- src/onyx.ts
+++ src/onyx.ts
@@ -1,6 +1,6 @@
 import SessionManager from './sessionManager.ts';
 import Strategy from './strategy.ts';
-import SessionStrategy from './strategies/session-strategy.ts';
+// import SessionStrategy from './strategies/session-strategy.ts';
 
 export default class Onyx {
   private _sm: any;
@@ -15,14 +15,9 @@ export default class Onyx {
   }
 
   init() {
-    this._sm = new SessionManager(
-      // this.serializeUser
-      this.serializeUser.bind(this)
-      /*, { key: this._key } */
-      // this.funcs
-    );
+    this._sm = new SessionManager(this.serializeUser.bind(this));
 
-    this.use(new SessionStrategy(this.deserializeUser.bind(this))); // not being used
+    // this.use(new SessionStrategy(this.deserializeUser.bind(this))); // not being used
   }
 
   // app.post('/login', passport.authenticate('local', {failureRedirect:'/login', successRedirect: '/dashboard'}))
@@ -43,7 +38,12 @@ export default class Onyx {
     return this;
   }
 
-  authenticate(strategy: string, options: any, callback: Function) {
+  // 11.5 - callback not being used, not seen in any example either.
+  authenticate(
+    strategy: string,
+    options?: { successRedirect?: string; failureRedirect?: string },
+    callback?: Function
+  ) {
     if (!strategy) {
       throw new Error(
         'You must provide an authentication strategy as an argment.'
@@ -59,19 +59,16 @@ export default class Onyx {
       );
     }
     console.log('hello from onyx.authenticate with currStrat', currStrat);
-    return currStrat.authenticate;
+    return currStrat.authenticate(this, options, callback);
+    // return currStrat.authenticate;
   }
 
   serializeUser(fn?: any, context?: any) {
+    console.log('in serializeUser of onyx, this.funcs is', this.funcs);
     if (typeof fn === 'function') {
       console.log('in serializeUser of onyx.ts');
       return (this.funcs.serializer = fn);
-    }
-    // else console.log('fn is user', fn);
-    // else return this.funcs.serializer;
-    // const user = fn;
-    else return this.funcs.serializer;
-    // console.log('this.funcs', this.funcs);
+    } else return this.funcs.serializer;
   }
 
   // onyx.deserializeUser((username, done) => {
@@ -85,7 +82,7 @@ export default class Onyx {
 
   // if session found in memory or redis,  adding session to ctx.state.onyx
   // when onyx is initialized in server 60, will check session db for session
-  initialize(onyx: any) {
+  initialize() {
     return async (context: any, next: Function) => {
       // each request should have it's own instance of Onyx
       // however we were not able to save the serializer and deserializer functions to each instance and had to pass in the server's onyx when calling for the authenticate function
@@ -130,9 +127,15 @@ export default class Onyx {
             // context.request.user = user
           }
         });
+        console.log('finishing deserializer in onyx');
       }
-      console.log('finishing deserialization in onyx');
+      console.log('finishing initialization in onyx');
       await next();
     };
+  }
+
+  // TODO
+  authorize(strategy: string, options?: any, callback?: Function) {
+    // for linking the 3rd party account to the account of a user that is currently athenticated
   }
 }
