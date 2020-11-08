e383db62bc4a91c2736864c4bc1dd60209645519
alicefu <69636893+alicejfu@users.noreply.github.com>
2020-11-08 15:49:58 -0500

    refractoring

--- src/onyx.ts
+++ src/onyx.ts
@@ -8,7 +8,6 @@ export default class Onyx {
   public funcs: any;
 
   constructor() {
-    // this.init()
     this._strategies = {};
     this.funcs = {};
     this.init();
@@ -16,29 +15,54 @@ export default class Onyx {
 
   init() {
     this._sm = new SessionManager(this.serializeUser.bind(this));
-
     // this.use(new SessionStrategy(this.deserializeUser.bind(this))); // not being used
   }
 
-  // app.post('/login', passport.authenticate('local', {failureRedirect:'/login', successRedirect: '/dashboard'}))
-
-  // *     passport.use(new TwitterStrategy(...));  // strategy.name = 'twitter'
-  // *     passport.use(new GitHubStrategy(...))
-  // *     passport.use('api', new http.BasicStrategy(...)); 'basic'  'api'
-
-  // onyx.authenticate('local', )
-
-  use(strategy: Strategy) {
-    const name = strategy.name;
-    if (!name) {
+  // 11.7 *options* - giving developer an option to customize their strategy name  *README*
+  use(name: string | Strategy, strategy?: Strategy) {
+    if (typeof name !== 'string') {
+      strategy = name;
+      name = strategy.name;
+    }
+    if (!name || typeof name !== 'string') {
       throw new Error('Authentication strategies must have a name!');
     }
-
     this._strategies[name] = strategy;
     return this;
   }
+  // onyx.authenticate('local')
+  // onyx.use('newLocal', new LocalStrategy())
 
-  // 11.5 - callback not being used, not seen in any example either.
+  // 11.7 - allows the developer to remove added strategies - not necessary in normal situations
+  unuse(name: string) {
+    delete this._strategies[name];
+    return this;
+  }
+
+  // 11.7 *note* callback function that is passed in is..? customized function to handle success/failure
+
+  /* Options:   *note* *README*
+   *   - `session`          Save login state in session, defaults to _true_
+   *   - `successRedirect`  After successful login, redirect to given URL
+   *   - `successMessage`   True to store success message in
+   *                        req.session.messages, or a string to use as override
+   *                        message for success.
+   *   - `failureRedirect`  After failed login, redirect to given URL
+   *   - `failureMessage`   True to store failure message in
+   *                        req.session.messages, or a string to use as override
+   *                        message for failure.
+   *   - `assignProperty`   Assign the object provided by the verify callback to given property
+   *
+   *
+   *     app.get('/protected', function(req, res, next) {
+   *       passport.authenticate('local', function(err, user, info, status) {
+   *         if (err) { return next(err) }
+   *         if (!user) { return res.redirect('/signin') }
+   *         res.redirect('/account');
+   *       })(req, res, next);
+   *     });
+   */
+
   authenticate(
     strategy: string,
     options?: { successRedirect?: string; failureRedirect?: string },
@@ -49,9 +73,6 @@ export default class Onyx {
         'You must provide an authentication strategy as an argment.'
       );
     }
-
-    // IMPORTANT the callback function being passed in seems to be for error handling, maybe we should just invoke it instead of throwing errors
-
     const currStrat = this._strategies[strategy];
     if (!currStrat) {
       throw new Error(
@@ -60,70 +81,67 @@ export default class Onyx {
     }
     console.log('hello from onyx.authenticate with currStrat', currStrat);
     return currStrat.authenticate(this, options, callback);
-    // return currStrat.authenticate;
   }
 
+  // 11.7 *note* does serializerUser ever take in context?
+  // 11.7 *note* if we can't access this function though binding, then make this just a simple storage function
   serializeUser(fn?: any, context?: any) {
-    console.log('in serializeUser of onyx, this.funcs is', this.funcs);
+    console.log('in serializeUser of onyx');
     if (typeof fn === 'function') {
-      console.log('in serializeUser of onyx.ts');
-      // return (this.funcs.serializer = fn);
-      this.funcs.serializer = fn;
-      return;
-    } else return this.funcs.serializer;
+      return (this.funcs.serializer = fn);
+    }
+    console.log('serializerUser invoked without function');
+    return this.funcs.serializer;
   }
 
   deserializeUser(fn: Function) {
     console.log('in deserializeUser of onyx.ts');
-    if (typeof fn === 'function') return (this.funcs.deserializer = fn);
-    else return this.funcs.deserializer;
+    if (typeof fn === 'function') {
+      return (this.funcs.deserializer = fn);
+    }
+    console.log('deserializerUser invoked without function');
+    return this.funcs.deserializer;
   }
 
-  // if session found in memory or redis,  adding session to ctx.state.onyx
-  // when onyx is initialized in server 60, will check session db for session
+  // when onyx is initialized in server (on each incoming request), will check session db for active session
+  // if active session found, invoke deserialization
   initialize() {
     return async (context: any, next: Function) => {
-      // each request should have it's own instance of Onyx
-      // however we were not able to save the serializer and deserializer functions to each instance and had to pass in the server's onyx when calling for the authenticate function
+      // each connection should have it's own instance of Onyx
+      // can only add property onto context.state which will persist while server is active, unlike the request object of express which is unique for each connection
       context.state.onyx = new Onyx();
-      // context.state.onyx = onyx;
 
-      // attempting to addi= logIn and logOut to context
-      // context.state.onyx._sm.logOut
+      // if we want to accomodate other frameworks, the following needs to be in an 'oak.tsx' file to be imported in as the framework
+
+      // adding the logIn and logOut to context.state for developer to invoke in the register and logout path
       context.state.logOut = context.state.logout = this._sm.logOut;
       context.state.logIn = context.state.login = this._sm.logIn;
 
-      console.log('logOut in ctx', context.state.logOut);
-      console.log('logIn in ctx', context.state.logIn);
-
-      //  {  SID1: {'userIDKey': userIDVal}
-      //     SID2: {}
-      //     SID3: {}
-      // }
-      // if session entry exist, load data from that
       const userIDVal = await context.state.session.get('userIDKey');
 
       // We won't enter this statement the first time we visit a website, since there isn't an existing session
       if (userIDVal) {
         if (!context.state.onyx.session) context.state.onyx.session = {};
         context.state.onyx.session.userID = userIDVal;
+
         console.log(
-          'session found! info saved in context.state.onyx.session',
+          'session found! userID saved in context.state.onyx.session',
           context.state.onyx.session
         );
 
-        // invoke deserializer using AWAIT so that it will wait for deserializer process to complete before sending response back to client
         await this.funcs.deserializer(userIDVal, function (
           err: any,
           user: any
         ) {
-          console.log('deserializer found user?', user);
           if (err) throw new Error(err);
-          else if (!user) delete context.state.onyx.session.userID;
-          else {
+          else if (!user) {
+            // so active session found but userIDVal does not return an user from userDB
+            delete context.state.onyx.session.userID;
+            // 11.7 *note* should we also delete the active session?
+          } else {
+            // user found in userDB, now store the info on onyx.session.user so developer can access it *FOR README*
             if (!context.state.onyx.session) context.state.onyx.session = {};
             context.state.onyx.session.user = user;
-            // context.request.user = user
           }
         });
         console.log('finishing deserializer in onyx');
