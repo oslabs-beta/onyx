8b6ba33391a8799224f26adcbebdbeb025d9d977
alicefu <69636893+alicejfu@users.noreply.github.com>
2020-11-09 11:00:19 -0500

    introducing authenticate middleware

--- example/server/routes.ts
+++ example/server/routes.ts
@@ -1,15 +1,41 @@
 import { Router } from '../deps.ts';
-import userController from './controllers/authController.ts';
-import sessionController from './controllers/sessionController.ts';
-// import users from './models/userModels.ts'
+import User from './models/userModels.ts';
 import onyx from '../../mod.ts';
 
 const router = new Router();
 
 // console.log('onyx in routes is', onyx);
-router.post('/register', userController.createUser, async (ctx) => {
-  await ctx.state.login();
+router.post('/register', async (ctx) => {
+  if (ctx.request.hasBody) {
+    const body = await ctx.request.body();
+    const { username, password } = await body.value;
+    const _id = await User.insertOne({ username, password });
+    console.log('id from database is', _id);
+    const user = { username, _id };
+
+    // 11.8 *note* need to invoke login with args
+    await ctx.state.logIn(ctx, user, onyx, async (err: any) => {
+      if (err) return ctx.throw(err);
+      else {
+        ctx.response.body = {
+          id: `id is ${_id}`,
+          message: `username is ${username}`,
+        };
+      }
+    });
+  } else {
+    ctx.response.body = {
+      success: true,
+      message: 'need input, body is empty',
+    };
+  }
 });
+
+// Passport  req.login(user, options?, function)
+// req.login(user, function(err) {
+//   if (err) { return next(err); }
+//   return res.redirect('/users/' + req.user.username);
+// });
 
 router.get('/login', async (ctx) => {
   ctx.response.body = {
