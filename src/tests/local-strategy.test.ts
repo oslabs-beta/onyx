a6adc0100e6b85e51c25fd2332580964b680b40e
alicefu <69636893+alicejfu@users.noreply.github.com>
2020-11-12 11:20:55 -0500

    more local-strategy tests

--- src/tests/local-strategy.test.ts
+++ src/tests/local-strategy.test.ts
@@ -2,13 +2,24 @@ import LocalStrategy from './../strategies/local-strategy/local-strategy.ts';
 import { describe, it, expect } from './../../test_deps.ts';
 
 describe('Local Strategy', () => {
-  let strategy = new LocalStrategy(function () {});
+  const verifyFunc = () => {
+    'verifyFunc';
+  };
+  const strategy = new LocalStrategy(verifyFunc);
 
-  it('should be named local', function () {
+  it('local-strategy should be named local', function () {
     expect(strategy.name).toEqual('local');
   });
 
-  it('error should throw if constructed without a verify callback', function () {
-    expect(strategy['_verify']).toBeDefined();
+  it('local-strategy should store the provided function in this._verify', function () {
+    expect(strategy['_verify']).toBe(verifyFunc);
+  });
+
+  it('local-strategy - usernameField should default to "username"', function () {
+    expect(strategy['_usernameField']).toEqual('username');
+  });
+
+  it('local-strategy - passwordField should default to "password"', function () {
+    expect(strategy['_passwordField']).toEqual('password');
   });
 });
