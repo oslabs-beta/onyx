979484b797993fc5ed93277b47fce7b8e9b363b5
alicefu <69636893+alicejfu@users.noreply.github.com>
2020-11-11 20:21:35 -0500

    exporting an instance of Onyx

--- mod.ts
+++ mod.ts
@@ -1,23 +1,2 @@
-// this will be the starting file for user to use our middleware
-// export everything important here
-
-import Authenticate from './src/onyx.ts';
-
-const Onyx = new Authenticate();
-
-export default Onyx;
-
-// exports = module.exports = new Passport();
-
-// /**
-//  * Expose constructors.
-//  */
-// exports.Passport =
-// exports.Authenticator = Passport;
-// exports.Strategy = require('passport-strategy');
-
-// /**
-//  * Expose strategies.
-//  */
-// exports.strategies = {};
-// exports.strategies.SessionStrategy = SessionStrategy;
+import Onyx from './src/onyx.ts';
+export default new Onyx();
